c9e516948450b598585d20990b44fd9d6519734f
DuwcsAnh1710 <75246anhhao@gmail.com>
2025-07-09 11:08:29 +0700

    update CameraUI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,4 +77,10 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.core:core-ktx:1.12.0")
+// CameraX
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
+// ML Kit Barcode Scanning
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
 }
